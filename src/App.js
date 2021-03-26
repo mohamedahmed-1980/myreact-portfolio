f4aa55849aab0a932a86aa4f654514021f0ac135
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-03-26 00:16:15 -0400

    routes path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route path="/" component={Header} />
+          <Route exact path="/" component={Main} />
           <Route path="/index.html" component={Main} />
           <Route path="/portfolio.html" component={Portofolio} />
           <Route path="/contact.html" component={Contacts} />
