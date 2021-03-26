4ad0c1c305bdb3887a5b32a01f527932de3c2413
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-03-26 00:12:24 -0400

    add Switch

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Main from "./components/Main";
 import Header from "./components/Header";
 import Portofolio from "./components/Portofolio";
 import Contacts from "./components/Contacts";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-        <Route path="/" component={Header} />
-        <Route path="/index.html" component={Main} />
-        <Route path="/portfolio.html" component={Portofolio} />
-        <Route path="/contact.html" component={Contacts} />
-
+        <Switch>
+          <Route path="/" component={Header} />
+          <Route path="/index.html" component={Main} />
+          <Route path="/portfolio.html" component={Portofolio} />
+          <Route path="/contact.html" component={Contacts} />
+        </Switch>
       </div>
     </Router>
 
